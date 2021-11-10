05f2bf305548307663ea2d6f666a36b19dd01bca
Shatyuka <958182453@qq.com>
2021-11-10 14:47:40 +0800

    Add more clients support

--- app/src/main/java/com/shatyuka/killergram/MainHook.java
+++ app/src/main/java/com/shatyuka/killergram/MainHook.java
@@ -1,5 +1,8 @@
 package com.shatyuka.killergram;
 
+import java.util.Arrays;
+import java.util.List;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
@@ -7,12 +10,21 @@ import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class MainHook implements IXposedHookLoadPackage {
+    public final static List<String> hookPackages = Arrays.asList(
+            "org.telegram.messenger",
+            "org.telegram.messenger.web",
+            "nekox.messenger",
+            "tw.nekomimi.nekogram",
+            "org.telegram.plus");
+
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) {
-        try {
-            Class<?> chatUIActivityClass = XposedHelpers.findClass("org.telegram.ui.ChatActivity", lpparam.classLoader);
-            XposedBridge.hookAllMethods(chatUIActivityClass, "addSponsoredMessages", XC_MethodReplacement.returnConstant(null));
-        } catch (Throwable ignored) {
+        if (hookPackages.contains(lpparam.packageName)) {
+            try {
+                Class<?> chatUIActivityClass = XposedHelpers.findClass("org.telegram.ui.ChatActivity", lpparam.classLoader);
+                XposedBridge.hookAllMethods(chatUIActivityClass, "addSponsoredMessages", XC_MethodReplacement.returnConstant(null));
+            } catch (Throwable ignored) {
+            }
         }
     }
 }
