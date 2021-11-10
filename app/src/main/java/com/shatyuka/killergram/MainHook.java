58c8f1c5475be926bb9a0639a6c317f01339059c
qingyiwebt <qingyiweb@gmail.com>
2021-11-10 13:37:20 +0800

    add support for Nekogram

--- app/src/main/java/com/shatyuka/killergram/MainHook.java
+++ app/src/main/java/com/shatyuka/killergram/MainHook.java
@@ -1,16 +1,20 @@
 package com.shatyuka.killergram;
 
+import java.util.Arrays; 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class MainHook implements IXposedHookLoadPackage {
-    public final static String hookPackage = "org.telegram.messenger";
+    public final static String[] hookPackages = new String[] {
+        "org.telegram.messenger",
+        "tw.nekomimi.nekogram"
+    };
 
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) {
-        if (hookPackage.equals(lpparam.packageName)) {
+        if (Arrays.stream(hookPackages).anyMatch(lpparam.packageName::equals)) {
             XposedHelpers.findAndHookMethod("org.telegram.ui.ChatActivity", lpparam.classLoader, "addSponsoredMessages", boolean.class, XC_MethodReplacement.returnConstant(null));
         }
     }
