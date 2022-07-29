ff77e662393cc1d899a9cdf6583b6102f0bc2f3b
void aire() <31969697+aire1@users.noreply.github.com>
2022-07-29 05:17:28 +0300

    Force set device performance class to high and etc. (#41)
    * Force set device performance class to high
    
    thats unlock some features (at least blur mode in navbar)
    
    * disable some restrictions
    
    extend accounts limit to 999 and unlock premium icons

--- app/src/main/java/com/shatyuka/killergram/MainHook.java
+++ app/src/main/java/com/shatyuka/killergram/MainHook.java
@@ -40,6 +40,15 @@ public class MainHook implements IXposedHookLoadPackage {
                 if (chatUIActivityClass != null) {
                     XposedBridge.hookAllMethods(chatUIActivityClass, "addSponsoredMessages", XC_MethodReplacement.returnConstant(null));
                 }
+                Class<?> SharedConfigClass = XposedHelpers.findClassIfExists("org.telegram.messenger.SharedConfig", lpparam.classLoader);
+                if (SharedConfigClass != null) {
+                    XposedBridge.hookAllMethods(SharedConfigClass, "getDevicePerformanceClass", XC_MethodReplacement.returnConstant(2));
+                }
+                Class<?> UserConfigClass = XposedHelpers.findClassIfExists("org.telegram.messenger.UserConfig", lpparam.classLoader);
+                if (UserConfigClass != null) {
+                    XposedBridge.hookAllMethods(UserConfigClass, "getMaxAccountCount", XC_MethodReplacement.returnConstant(999));
+                    XposedBridge.hookAllMethods(UserConfigClass, "hasPremiumOnAccounts", XC_MethodReplacement.returnConstant(true));
+                }
             } catch (Throwable ignored) {
             }
         }
