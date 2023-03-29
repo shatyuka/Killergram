7d24d784e7e8526c0ca53d31a4ca8ce92d63845b
Mukhammadsoli <usmanov.mukhammadsoli@gmail.com>
2023-03-29 13:36:42 +0300

    Add Telegraph support

--- app/src/main/java/com/shatyuka/killergram/MainHook.java
+++ app/src/main/java/com/shatyuka/killergram/MainHook.java
@@ -25,7 +25,8 @@ public class MainHook implements IXposedHookLoadPackage {
             "org.aka.messenger",
             "ellipi.messenger",
             "org.nift4.catox",
-            "it.owlgram.android");
+            "it.owlgram.android",
+            "ir.ilmili.telegraph");
 
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) {
